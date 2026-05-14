97adae75ff012c7383a54ee0b72b0d262f33dedb
Siwagorn <s66122202053@ssru.ac.th>
2026-05-14 16:22:43 +0800

    update cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: function(origin, callback) {
+    callback(null, true); // อนุญาต CORS จากทุก origin
+  },
   credentials: true,               // อนุญาตให้ส่ง Cookie/Headers พิเศษ
   methods: ["GET", "POST", "PUT", "DELETE"], // ระบุ Method ที่อนุญาต (เผื่อไว้)
   allowedHeaders: ["Content-Type", "Authorization"] // Header ที่ยอมรับ
